1d3ba0202ff5b51608315bd23c70d9eee73c3876
Gabriel Sousa <gabrielsilva.sousa2004@gmail.com>
2023-12-18 17:59:38 -0300

    18/12

--- resources/views/layouts/mainLayout.blade.php
+++ resources/views/layouts/mainLayout.blade.php
@@ -9,7 +9,9 @@
 </head>
 <body>
     <div class="content w-100 vh-100 d-flex justify-content-center align-items-center">
-        @yield('content') 
+        <div style="width: 10%;" class="h-100 bg-danger d-flex justify-content-center align-items-center">@include('components.sidebar')</div>
+        <div style="width: 90%;" class=" vh-100 d-flex justify-content-center align-items-center">@yield('content')</div>
     </div>
+    
 </body>
 </html>
