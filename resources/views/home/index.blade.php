ed9c0a04360799a9009e930d59e79a905fbc9e7e
hugo <carvalhohugo425@gmail.com>
2023-12-19 23:49:54 -0300

    Dashboard feita

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -3,11 +3,50 @@
 @section('title','pagina login')
 
 @push('css')
-<link rel="stylesheet" href="{{ URL::asset('css/app.css') }}" type="text/css">
+<link rel="stylesheet" href="{{ URL::asset('css/dashboard.css') }}" type="text/css">
 @endpush
     
 @section('content')
-    <span class="display-1">DASHBOARD</span>
+    <div id="sectionPrincipal" class="container  d-flex flex-column alig-items-center justify-content-between">
+        <div style="height: 45%" class="w-100   d-flex flex-row justify-content-center align-items-center">
+            <div class="item1 d-flex flex-column justify-content-between align-items-center   h-100">
+                <div class="w-100 text-center"><span class="fs-2" style="font-weight: 400">Usuarios Registrados</span></div>
+                <div class="w-100 text-center"><span class="display-4 fw-bolder">459</span></div>
+                <div class="w-100"></div>
+
+            </div>
+            <div class="item2 d-flex flex-column justify-content-between align-items-center   h-100">
+                <div class="w-100 text-center"><span class="fs-2" style="font-weight: 400">Recargas Efetuadas</span></div>
+                <div class="w-100 text-center"><span class="display-4 fw-bolder">459</span></div>
+                <div class="w-100"></div>
+
+            </div>
+            <div class="item3 d-flex flex-column justify-content-between align-items-center   h-100">
+                <div class="w-100 text-center"><span class="fs-2" style="font-weight: 400">Fluxo De Viagens(Diárias)</span></div>
+                <div class="w-100 text-center"><span class="display-4 fw-bolder">"Grafico em linha"</span></div>
+                <div class="w-100"></div>
+                
+            </div>
+            
+        </div>
+        <div style="height: 45%" class="separator w-100   d-flex flex-row justify-content-center align-items-center">
+            <div class="item h-100 d-flex flex-column justify-content-between align-items-center">
+                <div class="w-100 text-center"><span class="fs-2" style="font-weight: 400">Faturamentos</span></div>
+                <div class="w-100 text-center"><span class="display-4 fw-bolder">R$1.000.000,00</span></div>
+                <div class="w-100"></div>
+            </div>
+            <div style="border-left: 1px solid #FF0000" class="item h-100 d-flex flex-column justify-content-between align-items-center">
+                <div class="w-100 text-center"><span class="fs-2" style="font-weight: 400">Catracas em atividade</span></div>
+                <div class="w-100 text-center"><span class="display-4 fw-bolder">459</span></div>
+                <div class="w-100"></div>
+            </div>
+        </div>
+        
+    </div>
 @endsection
 
+
+
 @section('pageTitle', 'Painel de Controle')
+
+
--- resources/views/layouts/mainLayout.blade.php
+++ resources/views/layouts/mainLayout.blade.php
@@ -7,6 +7,8 @@
     @stack('css')
     <link rel="stylesheet" href="{{ URL::asset('css/app.css') }}">
     <script src="https://kit.fontawesome.com/d5ea0dfb99.js" crossorigin="anonymous"></script>
+    
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title>@yield('title') laravel</title>
 </head>
@@ -21,4 +23,6 @@
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 <script>$('.dropdown-toggle').dropdown()</script>
+
+
 </html>
