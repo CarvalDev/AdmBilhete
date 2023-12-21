9967ee6d84bba906bec1f59dc762b2b949266899
Mateus707 <darkp331@gmail.com>
2023-12-21 14:51:30 -0300

    atualizacão da responsividade do login

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -2,7 +2,11 @@
 
 <dialog style="height: 60vh; width: 35vw" id="modal">
     <div class="h-100 w-100">
-        
+        <div class="d-flex justify-content-end">
+            <button id="btClosePerfil" type="button" class="btn-close btn-white" data-bs-dismiss="modal" aria-label="Close"></button>
+                
+        </div>
+        <hr style="border: 0.1px solid gray" class="my-3">
     </div>
     
 </dialog>
--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -1,25 +1,29 @@
 @extends('layouts.mainLayoutLogin')
 
 @section('title')
-    
+
+@push('css')
+<link rel="stylesheet" href="{{ URL::asset('css/login.css')}}">
 @section('content')
 <div class="row h-100 w-100 m-0 p-0">
-    <div class="col-md-6 d-flex justify-content-center align-items-center">
-        <div class="border border-black rounded-4 p-5 w-50 h-50">
+    <div class="col-md-6 col-xs-12 d-flex justify-content-center align-items-center">
+        <div class="border border-black rounded-4 w-50 h-50">
+            <div class="p-xl-3 p-md-3 p-xxl-5 p-sm-1 p-xs-5 p-lg-4 testeResposivo testeResposivoMenor">
             <form action="" class="w-100">
-                 <label for="email" class="form-label">Email</label>
-                <input type="email" name="email" id="email" class="form-control mb-3">
-                <label for="senha" class="form-label ">Senha</label>
-                <input type="password" name="senha" class="form-control mb-3" id="senha">
+                 <label for="email" class="form-label fw-bold text-xs">Email</label>
+                <input type="email" name="email" id="email" class="form-control mb-xl-3 mb-sm-3">
+                <label for="senha" class="form-label fw-bold text-xs ">Senha</label>
+                <input type="password" name="senha" class="form-control mb-xl-3 mb-sm-3" id="senha">
                 <input type="checkbox" name="checkbox" id="checkbox">
-                <label for="checkbox" class="mb-3 mt-3" >Lembre de mim</label>
-                <input type="button" class="fundo fw-bold text-uppercase mt-3 w-100 btn btnCss" value="Enviar">
+                <label for="checkbox" class=" fw-bold text-xs" >Lembre de mim</label>
+                <input type="button" class="fundo fw-bold text-uppercase w-100 btn btnCss mt-xl-3 mt-sm-5" value="Enviar">
             </form>
+            </div>
         </div>
     </div>
-<div class="col-md-6 d-sm-none d-md-block m-0 p-0">
+<div class="col-md-6 col-xs-12 d-sm-none d-md-block m-0 p-0">
     <div class="d-none d-md-flex h-100  justify-content-center align-items-center fundo">
-        <img src="{{ url('storage/site/logo bilhete 1.png') }}" class="img-fluid " alt="" srcset="">
+        <img src="{{ url('storage/site/logo bilhete 1.png') }}" class="img-fluid" alt="" srcset="">
     </div>
   
     
