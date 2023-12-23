1828956089e9a11c3b8f7b233b4e59fc67cb96fa
Gabriel Sousa <gabrielsilva.sousa2004@gmail.com>
2023-12-23 15:42:04 -0300

    23/12

--- resources/views/components/barraPesquisa.blade.php
+++ resources/views/components/barraPesquisa.blade.php
@@ -1,7 +1,7 @@
 <form action="" method="POST" class="d-flex flex-row">
     @csrf
-    <input type="text" placeholder="Pesquisa" name="search" class="form-control">
-    <button type="submit" style="background: none; border:none;font-size:22px"><i style="color: #ff0000" class="fa-solid fa-magnifying-glass"></i></button>    
+    <input type="text" placeholder="Pesquisa" name="search" class="form-control border border-danger rounded-5">
+    <button type="submit" style="background: none; border:none;font-size:22px"><i style="color: #000" class="fa-solid fa-magnifying-glass"></i></button>    
 </form>
 
 
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,31 +1,32 @@
-<div style="height:20%; width:80%" class="d-flex flex-column justify-content-between align-items-center">
-    <div class="d-flex flex-row w-100  justify-content-between align-items-center h-100">
+<div style="height:20%; width:100%" class="d-flex flex-column justify-content-between align-items-center">
+    <div class="d-flex flex-row w-100  justify-content-around align-items-center h-100">
         <div style="width: 10%" class=""></div>    
-        <div style="width: 30%" class="">
+        <div style="width: 40%" class="">
         
         @yield('pesquisa')
         
         </div>   
-        <div style="cursor:pointer;" class="dropdown-toggle d-flex  flex-row  h-100 justify-content-end  gap-2 align-items-center"  data-bs-toggle="dropdown" aria-expanded="false">
+        <div style="cursor:pointer;" class="dropdown-toggle d-flex  flex-row justify-content-end  gap-1 align-items-center"  data-bs-toggle="dropdown" aria-expanded="false">
             <div class="d-flex justify-content-center align-items-center  ">
-                <img src="{{ url('storage/site/user.png') }}" width="30px" alt="">
+                <img src="{{ url('storage/site/user.png') }}" width="40px" alt="">
             </div>
-            <ul class="dropdown-menu text-small">
+            <ul class="dropdown-menu text-small" >
             <li><button class="dropdown-item">Pefil</button></li>
             <li><button class="dropdown-item">Sign out</button></li>
             </ul>
             <div class="d-flex flex-column align-items-center justify-content-center">
-                
-                <span class="ms-4 fw-bold fs-5">ADM</span>     
+                <span class="ms-4 fw-bold">Nome do Admin</span>     
                 <span id="cargo" class="ms-4">cargo</span>
                 
             </div>
         </div>
     </div>
-    <div id="pageTitleContainer" class="d-flex justify-content-start w-100 align-items-center">
-        <span id="pageTitle" class="fs-3 fw-bold">@yield('pageTitle')</span>
-        <div id="underline"></div>
-      </div>
+    <div class="container">
+        <div id="pageTitleContainer" class="d-flex  justify-content-start w-100 align-items-center">
+            <span id="pageTitle" class="fs-3 fw-bold">@yield('pageTitle')</span>
+            <div id="underline"></div>
+        </div>
+    </div>
       
 </div>
 
