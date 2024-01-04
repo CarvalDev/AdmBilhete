9416350ef0a382d29e46453461a7a9f7ad77816d
hugo <carvalhohugo425@gmail.com>
2024-01-03 21:23:36 -0300

    short icon

--- resources/views/layouts/mainLayout.blade.php
+++ resources/views/layouts/mainLayout.blade.php
@@ -7,7 +7,7 @@
     @stack('css')
     <link rel="stylesheet" href="{{ URL::asset('css/app.css') }}">
     <script src="https://kit.fontawesome.com/d5ea0dfb99.js" crossorigin="anonymous"></script>
-    
+    <link rel="short icon" href="{{ URL::asset('storage/site/logo bilhete 1.png') }}" />
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title>@yield('title') - BUD</title>
