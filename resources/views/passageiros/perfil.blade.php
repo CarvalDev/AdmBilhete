5611a1997dc8d04bd9bc8bdc0d637892c6ce55ae
Miguel <migramoslim@gmail.com>
2024-01-06 19:10:05 -0300

    mudança mínima na area do perfil do passageiro

--- resources/views/passageiros/perfil.blade.php
+++ resources/views/passageiros/perfil.blade.php
@@ -11,25 +11,61 @@
 @section('content')
 <div class="w-100 h-100  d-flex flex-row">
     <div class="infosPassageiro h-100" style="width:70%">
-        <div class="primeirasInfo ml-5  d-flex flex-column justify-content-center align-items-center w-50 h-50 d-flex flex-row">
-            <div class="fotoPassageiro" style="height:50%; border: 2px solid gray; border-radius:8%"  >
+        <div class="primeirasInfo ml-5  d-flex flex-column justify-content-center align-items-center w-50 d-flex flex-row" style="height:45%">
+            <div class="fotoPassageiro" style="height:70%; border: 2px solid gray; border-radius:8%"  >
                 <img src="{{ url("storage/$passageiro->fotoPassageiro") }}" class="w-100 h-100" style="border-radius: 8%" alt="">
-                
             </div>
             <div class="dadosPrincipais d-flex flex-column ph-4">
                 <div class="flex-row d-flex gap-2 justify-content-center"><p style="color:rgb(52, 49, 49);" class="fw-bold p-0 m-0">{{ $passageiro->nomePassageiro }}</p></div>
                 <div class="flex-row d-flex gap-2 justify-content-center"><p  style="font-size: 12px" class="fw-bold text-secondary  p-0 m-0 mb-3 text-center">{{ $passageiro->cpfPassageiro }}</p></div>
-                <div class="d-flex flex-row justify-content-center align-items-center w-100 gap-3">
-                    <div class="d-flex gap-2"><strong class="fs-5">Total de recargas:</strong><p class="fw-bold fs-5">2</p>
-                    </div>
-                    <button class="p-0 mb-3 border-0 "><i class="fa-regular fa-pen-to-square fa-xl p-0 m-0"></i></button>
-                </div>
             </div>
         </div>
 
-        <div class="grafico w-100 h-50 align-items-center justify-content-center d-flex ">
-            <div class="rounded-3" style="height: 85%;width:65%;border: 2px solid red">
-                <p>grafico</p>
+        <div class="bilhetes w-100  align-items-center justify-content-center  d-flex flex-column" style="height: 55%">
+            <div class="rounded-4" style="height: 95%;width:60%;background-color:#1E90FF">
+                <header class="w-100  d-flex flex-row justify-content-between" style="height:21%">
+                    <div class="dNe h-100 d-flex align-items-center justify-content-center" style="width:35%">
+                        <img src=" {{ url('images/DNElogo.png')}} " style="width: 85%;height:80%">
+                    </div>
+                    <div class="logos h-100  d-flex justify-content-center align-items-center gap-3" style="width:65%">
+                        <img src=" {{ url('images/UNElogo.png')}} " style="width: 13%;height:70%">
+                        <img src=" {{ url('images/UBESlogo.png')}} " style="width: 13%;height:70%">
+                        <img src=" {{ url('images/ANPGlogo.png')}} " style="width: 22%;height:48%">
+                        <img src=" {{ url('images/UEElogo.png')}} " style="width: 13%;height:70%">
+                        <img src=" {{ url('images/UMESlogo.jpg')}} " style="width: 13%;height:70%">
+                    </div>
+                </header>
+
+                <section class="corpoBilhete w-100  d-flex flex-row" style="height:55%">
+                        <div class="fotoDonoBilhete  h-100" style="width:30%;margin-left:3%">
+                            <img src=" {{ url('images/iconFotoBilhete.jpg')}}" class="h-100 w-100">
+                        </div>
+
+                        <div class="infosBilhete justify-content-center gap-1 h-100 d-flex flex-column p-2 px-3" style="width:70%">
+                            <strong style="font-size:13px">MIGUEL R. LIMA</strong>
+                            <label style="font-size:13px">ESCOLA TÉCNICA ESTADUAL DE GUAIANAZES</label>
+                            <label style="font-size:13px">TÉCNICO</label>
+                            <div class="" style="font-size:13px"><strong>CPF</strong><label class="ps-1">{{ $passageiro->cpfPassageiro }}</label></div>
+                            <div class="" style="font-size:13px"><strong>Data Nasc.</strong><label class="ps-1">{{ $passageiro->dataNascPassageiro }}</label></div>
+                            <div class="" style="font-size:13px"><strong>Matrícula</strong><label class="ps-1">012345789</label></div>
+                        </div>
+                </section>
+                
+                <footer class="rodapeBilhete w-100 d-flex flex-row" style="height: 24%">
+                    <div class="QRCode  h-100 alig-items-center justify-content-center d-flex" style="width:25%;margin-left:3%">
+                     
+                    </div>
+                    <div class="codUsoBilhete h-100 p-2 px-4 d-flex flex-row gap-5 h-100" style="width:75%">
+                        <div class="" style="width: 70%">
+                            <label style="font-size:12px">Código de uso/N° do Bilhete Único</label>
+                            <p class="fw-bold" style="padding-left:47%;font-size:13px">1 234 567 890</p>
+                        </div>
+
+                        <div class="logo2023 d-flex justify-content-center align-items-center h-100" style="width:30%">
+                            <img src=" {{ url('images/anoBilhete.png')}} " style="width: 110%;height:170%">
+                        </div>
+                    </div>
+                </footer>
         
             </div>
         </div>
