df5263ebedee621dbd1acda8e54f9cf998aae6a1
Miguel <migramoslim@gmail.com>
2024-02-25 00:27:49 -0300

    nova tela loginAdm

--- resources/views/layouts/mainLayoutLogin.blade.php
+++ resources/views/layouts/mainLayoutLogin.blade.php
@@ -10,7 +10,13 @@
    
 </head>
 <body>
-    <div class="container-fluid h-100 m-0 p-0">
+    <div style="font-family: 'Poppins', sans-serif;
+    height: 100vh;
+    width: 100vw;
+    background-color: #dac8c8;
+    display:flex;
+    justify-content: center;
+    align-items: center;">
         @yield('content')
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -5,34 +5,32 @@
 @push('css')
 <link rel="stylesheet" href="{{ URL::asset('css/login.css')}}">
 @section('content')
-<div class="row h-100 w-100 m-0 p-0">
-    <div class="col-md-6 col-xs-12 d-flex justify-content-center align-items-center">
-        <div class="border border-black rounded-4 w-50 h-50">
-            <div class="p-xl-3 p-md-3 p-xxl-5 p-sm-1 p-xs-5 p-lg-4 testeResposivo testeResposivoMenor">
-            <form action="{{ route('adm.login') }}" class="w-100" method="POST">
-                @csrf
-                <label for="email" class="form-label fw-bold text-xs">Email</label>
-                <input type="emailAdm" name="emailAdm" id="email" class="form-control mb-xl-3 mb-sm-3">
-                <label for="senha" class="form-label fw-bold text-xs ">Senha</label>
-                <input type="password" name="password" class="form-control mb-xl-3 mb-sm-3" id="senha">
-                <input type="checkbox" name="checkbox" id="checkbox">
-                <label for="checkbox" class=" fw-bold text-xs" >Lembre de mim</label>
-                <input type="submit" class="fundo fw-bold text-uppercase w-100 btn btnCss mt-xl-3 mt-sm-5" value="Enviar">
-                @if (session('erro'))
-                    <div class="w-100 mt-2  text-center">
-                        <span class="text-danger">{{session('erro')}}</span>
-                    </div>
-                @endif
-            </form>
-            </div>
-        </div>
+<div class="card">
+    <form action=" {{ route('adm.login') }}" class="form" method="POST">
+        @csrf
+      <h3>Login</h3>
+      <div class="input-field">
+        <i class="fa-solid fa-envelope"></i>
+          <input type="text" name="emailAdm" placeholder="Enter your email">
+      </div>
+      <div class="input-field">
+        <i class="fa fa-lock"></i>
+        <input type="password" name="password" placeholder="Enter your password">
     </div>
-<div class="col-md-6 col-xs-12 d-sm-none d-md-block m-0 p-0">
-    <div class="d-none d-md-flex h-100  justify-content-center align-items-center fundo">
-        <img src="{{ url('images/logo bilhete 1.png') }}" class="img-fluid" alt="" srcset="">
+    @if (session('erro'))
+    <div class="w-100 fw-bold p-3">
+        <span class="text-danger">{{session('erro')}}</span>
     </div>
-  
-    
-</div>     
-</div>
+@endif
+    <div class="p-3"></div>
+    <button type="submit">Entrar</button>
+    <p>Não tem conta? <a href="#">KKKKKKKK</a></p>
+</form>
+    <div class="image">
+      <div class="overlay">
+        <img src="{{ URL::asset('images/logobranca.png') }}" id="imagemB">
+      </div>
+    </div> 
+  </div>
+   
 @endsection
